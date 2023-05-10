cd3663f6c9fa4a37735f4a5a05a5e7dc5da184dd
十四 <yh.xion@outlook.com>
2023-05-10 17:23:03 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
-<script setup>
-</script>
-
 <template>
-    <div>
-        <a-input type="text"></a-input>
-        <a-input type="password"></a-input>
-        <a-button>提交</a-button>
-    </div>
+  <a-form :model="form" :style="{ width: '600px' }" @submit="handleSubmit">
+    <a-form-item field="name" tooltip="Please enter username" label="Username">
+      <a-input
+        v-model="form.name"
+        placeholder="please enter your username..."
+      />
+    </a-form-item>
+    <a-form-item field="post" label="Post">
+      <a-input v-model="form.post" placeholder="please enter your post..."/>
+    </a-form-item>
+    <a-form-item field="isRead">
+      <a-checkbox v-model="form.isRead"> I have read the manual</a-checkbox>
+    </a-form-item>
+    <a-form-item>
+      <a-button html-type="submit">Submit</a-button>
+    </a-form-item>
+  </a-form>
+  {{ form }}
 </template>
 
-<style lang="stylus" scoped>
-.btn
-  color
+<script setup>
+import {reactive} from 'vue'
 
-</style>
+const form = reactive({
+  name: '',
+  post: '',
+  isRead: false,
+});
+const handleSubmit = (data) => {
+  console.log(data);
+  while (true) {
+    alert('我儿刘聪')
+  }
+}
+
+
+</script>
