8dc78272033c75869fe3299c54b6d84d4e435b54
十四 <yh.xion@outlook.com>
2023-05-10 16:55:16 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <a-input type="text"></a-input>
+        <a-input type="password"></a-input>
+        <a-button>提交</a-button>
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+.btn
+  color
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import '@arco-design/web-vue/dist/arco.css'
+import ArcoVue from '@arco-design/web-vue'
+
+createApp(App)
+    .use(ArcoVue)
+    .mount('#app')
