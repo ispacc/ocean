88a73ec25f3e454d66ea36f1c2a39ac7ca6fa4e8
十四 <yh.xion@outlook.com>
2023-07-10 17:50:17 +0800

    :sparkles: Introducing new features.

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtLink to="/about">about</NuxtLink>
+    |
+    <NuxtLink to="/">根</NuxtLink>
+    |
+    <NuxtLink to="/parent-1">嵌套路由页面</NuxtLink>
+    <slot/>
+  </div>
+</template>
