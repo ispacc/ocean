1fd2758bcb53798d0995ae0b8808ef8d70798ffa
ispacc <yh.xion@outlook.com>
2023-07-12 00:37:22 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-
+  <div class="h-screen">
+    <div class="flex items-center font-serif justify-center text-2xl">
+      <NuxtLink to="/">Home</NuxtLink>
+      |
+      <NuxtLink to="/demo">Demo</NuxtLink>
+    </div>
     <slot/>
   </div>
 </template>
